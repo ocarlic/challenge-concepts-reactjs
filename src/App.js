6ff8270b4faa655a9e5a43486e9623d12c78deaa
Victor Carli <victorcarlibr@gmail.com>
2020-04-12 15:11:10 -0300

    finished challenge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: "Front End com GoStack",
+      url: "https://github.com/ocarlic",
+      techs: ["Node", "Express", "ReactJS", "React Native", "Angular", "Vue", "Golang", "Javascript", "Jest"]
+    })
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`repositories/${id}`);
+
+    const newRepository = repositories.filter(repository => repository.id !== id);
+
+    setRepositories(newRepository);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
+      {repositories.map(({ title, id, techs }) => (
+        <li key={id}>
+          <span>
+            <p className="title">{title}</p>
+            <span className="tag-techs">
+              {techs.map(tech => <span className="tag-techs-items" key={tech}>{tech}</span> )}
+            </span>
+          </span>
+          
+          <button onClick={() => handleRemoveRepository(id)}>
             Remover
           </button>
         </li>
+      ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
